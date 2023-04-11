3bf578ca0c82362fcdbe8ea07d87fb4734ebe4d9
Haddy Alchaer <halchaer@purdue.edu>
2023-04-11 18:29:21 -0400

    plotting

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import qiskit
+from qiskit.circuit import QuantumCircuit, Parameter
+import matplotlib.pyplot as plt
 
+theta = Parameter('θ')
+
+qc = QuantumCircuit(2)
+qc.rz(theta, 0)
+qc.crz(theta, 0, 1)
+fig = qc.draw(output="mpl")
+
+# Display the figure in a plot window
+plt.show()
